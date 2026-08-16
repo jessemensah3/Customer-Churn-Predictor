3574ab2a3657cad25a5fa98f18154f52d7b17c32
Jesse Mens <jessemensah3@gmail.com>
2026-08-16 18:58:09 +0000

    Add customer churn model training

--- train_model.py
+++ train_model.py
@@ -3,7 +3,6 @@ import joblib
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import (
     accuracy_score,
